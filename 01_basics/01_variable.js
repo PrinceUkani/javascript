44be872aae726bf20574a8296cf790fc66d845f6
PrinceUkani <princeukani15@gmail.com>
2026-01-02 15:37:14 +0000

    datatype

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,18 @@
+const account = 124
+let accountemail = "Prince@.com"
+var asdg = "12854"
+city = "Surat"
+
+
+//account = 8
+
+console.log(account);
+
+accountemail = "Ukani"
+asdg = "487"
+city = "Ahmedabad"
+
+console.log(accountemail);
+console.table([account, accountemail , asdg , city]);
+
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,9 @@
+"Use strict " // newer version of java script
+let name = "Prince"
+let age = 32
+let married = false     
+//null
+let number; //undefined
+
+console.log(typeof married);
+console.log(typeof age);
